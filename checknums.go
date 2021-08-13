6a566864f5467a72c677a1f05a56feace9e83c05
PRAJWAL PATIL <46968147+prajwal-scorpionking123@users.noreply.github.com>
2021-08-13 14:03:18 +0530

    v2

--- checknums.go
+++ checknums.go
@@ -7,3 +7,23 @@ func CheckEven(number int) bool {
 		return false
 	}
 }
+
+func CheckPalindrome(number int) bool {
+	var remainder, temp int
+	var reverse int = 0
+	for {
+		remainder = number % 10
+		reverse = reverse*10 + remainder
+		number /= 10
+
+		if number == 0 {
+			break // Break Statement used to exit from loop
+		}
+	}
+
+	if temp == reverse {
+		return true
+	} else {
+		return false
+	}
+}
